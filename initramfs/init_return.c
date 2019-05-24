7b66852f5682bad21fd7a9e45e69c59f239ae970
Gabriel Amorim <gabriel.rocha.amorim@usp.br>
2019-05-24 00:04:15 -0300

    Arquivo init que retorna, gerando kernel panic

---
+++ initramfs/init_return.c
@@ -0,0 +1,26 @@
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void print_file() {
+    int fp;
+    fp = open("text.txt", O_RDONLY);
+    if(fp < 0)
+    {
+        write(1, "Error\n", 6);
+        return;
+    }
+    char c[1];
+    while(read(fp, &c, 1))
+    {
+        write(1, &c, sizeof(c));
+    }
+    close(fp);
+
+}
+int main() {
+    print_file();    
+	
+    return 0;
+}
